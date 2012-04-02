194874587168a9c3740aaf13d3ca63292f19feb6
Brian Carpio <bcarpio@thetek.net>
2012-04-02 13:31:42 -0700

    apparently I didn't have the config file added to git

---
+++ public_html/index.php
@@ -0,0 +1,30 @@
+<?php
+// load up your config file
+    require_once("../conf/config.php");
+	require_once($TEMPLATE_PATH . "/header.php");
+?>
+
+<?php
+$list = $db->listCollections();
+
+foreach ($list as $collection){
+	$collection = preg_replace("/".$DATABASE."/", $replace, $collection);
+	if(!isset($_GET['nosql'])) {
+?>
+
+<a href="./index.php?nosql=<?php echo "$collection" ?>"><?php echo "$collection"?></a> 
+
+<?php
+}
+}
+
+if(isset($_GET['nosql'])) {
+	$s = $_GET["nosql"];
+?>
+	<B><?php print $s ?></B>
+<?php
+}
+?>
+<?php
+	require_once($TEMPLATE_PATH . "/footer.php");
+?>
